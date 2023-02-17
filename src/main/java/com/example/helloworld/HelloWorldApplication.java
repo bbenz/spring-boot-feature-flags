5d76728bdbfbbde3f6ff833cf8a2a1b1e09d43aa
Brian Benz <bbenz@microsoft.com>
2023-02-16 20:35:36 -0800

    Rearranging the deck chairs

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootApplication
 @EnableConfigurationProperties(MessageProperties.class)
-//@ComponentScan("helloworld")
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/helloworld/HelloWorldFeatures.java
+++ src/main/java/com/example/helloworld/HelloWorldFeatures.java
@@ -1,31 +1,47 @@
 package com.example.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.azure.spring.cloud.feature.manager.FeatureManager;
 
-@RestController
-@ConfigurationProperties("controller")
+//@RestController
+@Controller
+@ConfigurationProperties
+//@ConfigurationProperties("controller")
+
+
+
 public class HelloWorldFeatures {
-    
-    //@Autowired
-    public FeatureManager featureManager;
 
-    public HelloWorldFeatures(FeatureManager featureManager) {
-        this.featureManager = featureManager;
-     }
+@Autowired
+private FeatureManager featureManager;
 
-    @GetMapping("/feature")
-    public String getFeature() {
-    //  Retrieve the value of the "Beta" feature flag
-        boolean myFeatureEnabled = featureManager.isEnabledAsync("Beta").block();
+//@Autowired
+//FeatureManager featureManager;
+
+public HelloWorldFeatures(FeatureManager featureManager) {
+    this.featureManager = featureManager;
+}
+
+@GetMapping("/feature")
+public String getFeature() {
+    //     Retrieve the value of the "Beta" feature flag
+        boolean myFeatureEnabled = featureManager.isEnabledAsync("featureManagement.Beta").block();
+//        boolean myFeatureEnabled = featureManager.isEnabledAsync("Beta").block();
         if (myFeatureEnabled) {
             return "My feature is enabled!";
         } else {
             return "My feature is disabled.";
+//            return "My feature is not thereyet.";
         }
-    }
 
 }
+}
+
+    
+    
+    
+
