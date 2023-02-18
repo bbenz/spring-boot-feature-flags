7a1dc89df131926c5c0ac0beccaddaf67ba1a3ec
Brian Benz <bbenz@microsoft.com>
2023-02-17 21:13:15 -0800

    Working version!   Thanks Matthew Metcalf

--- src/main/java/com/example/helloworld/HelloWorldFeatures.java
+++ src/main/java/com/example/helloworld/HelloWorldFeatures.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.azure.spring.cloud.feature.manager.FeatureManager;
 
 @RestController
-//@Controller
-//@ConfigurationProperties("controller")
 public class HelloWorldFeatures {
     private FeatureManager featureManager;
 
@@ -16,18 +14,12 @@ public class HelloWorldFeatures {
     }
 
     @GetMapping("/feature")
-    public String getFeature() {
-        //     Retrieve the value of the "Beta" feature flag
-//        boolean myFeatureEnabled = featureManager.isEnabledAsync("featureManagement.Beta").block();
-//        boolean myFeatureEnabled = featureManager.isEnabledAsync("featureManagement.Beta").block();
-//        if (myFeatureEnabled) {
-    
-    //if(featureManager.isEnabledAsync("featureManagement.Beta").block()) {
+    public String getFeature()  {
+    //     Retrieve the value of the "Beta" feature flag
     if(featureManager.isEnabledAsync("Beta").block()) {
             return "My feature is enabled!";
         } else {
             return "My feature is disabled.";
-//            return "My feature is not there yet.";
         }
     }
 }
